cdec2b24d52ddcdfd616412b14a2eb86ed60962b
bromix <bromix@gmx.net>
2014-09-19 23:58:07 +0200

    moved to testing shelve for storing favs data.

--- lets_cook.py
+++ lets_cook.py
@@ -1,7 +1,7 @@
 root_url = 'https://github.com/bromix/'
 
 resources = [{'name': 'kodimon',
-              'branch': 'master'},
+              'branch': 'shelve_test'},
              {'name': 'plugin.video.bromix.youtube',
               'branch': 'master'},
              {'name': 'plugin.picture.bromix.break',
