0cd2da48e0ec51dadc4d2cb8df3cc5362d4dbb7a
bromix <bromix@gmx.net>
2014-10-14 19:24:49 +0200

    UPD: SoundCloud for tests (explore)

--- lets_cook.py
+++ lets_cook.py
@@ -1,7 +1,7 @@
 root_url = 'https://github.com/bromix/'
 
 resources = [{'name': 'plugin.audio.bromix.soundcloud',
-              'branch': 'master'},
+              'branch': 'explore'},
              {'name': 'plugin.video.bromix.youtube',
               'branch': 'master'},
              {'name': 'plugin.picture.bromix.break',
