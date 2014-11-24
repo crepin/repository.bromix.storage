543228403392ab437ff87675e0f307fb6d5cf315
bromix <bromix@gmx.net>
2014-11-24 18:50:30 +0100

    UPD: SoundCloud

--- lets_cook.py
+++ lets_cook.py
@@ -7,13 +7,11 @@ resources = [{'name': 'plugin.video.9gagtv', 'branch': 'master'},
              {'name': 'plugin.picture.bromix.break', 'branch': 'master'},
              {'name': 'plugin.video.bromix.tlc_de', 'branch': 'master'},
              {'name': 'plugin.video.bromix.dmax_de', 'branch': 'master'},
-
-             # pending
              {'name': 'plugin.video.netzkino_de', 'branch': 'master'},
 
              # bromix repository
              {'name': 'plugin.video.bromix.myvideo_de', 'branch': 'master'},
-             {'name': 'plugin.video.bromix.youtube', 'branch': 'master'},
+             #{'name': 'plugin.video.bromix.youtube', 'branch': 'master'},
              {'name': 'plugin.video.bromix.break', 'branch': 'master'},
              {'name': 'plugin.video.bromix.rtl_now', 'branch': 'master'},
              {'name': 'plugin.video.bromix.rtl2_now', 'branch': 'master'},
